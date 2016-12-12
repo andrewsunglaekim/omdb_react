655abe612d1b8092f0d0e724d1b8e92e462ce966
Andy <andrew.sunglae.kim@gmail.com>
2016-12-12 16:00:55 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Search from './Search'
+
+class Home extends Component{
+  render(){
+    return (
+    <main>
+      <h1>OMDB React</h1>
+      <Search />
+    </main>
+    )
+  }
+}
+
+export default Home
---
+++ src/Search.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Search extends Component{
+  render() {
+    return(
+      <div>
+        This is the Search Component
+        arewogiaeg:w
+
+      </div>
+    )
+  }
+}
+
+export default Search
